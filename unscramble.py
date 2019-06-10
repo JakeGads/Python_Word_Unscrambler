30fa80e389a6f07c97d566be2fcad2ca9a3a88d9
Jake Gadaleta <jake.gads@gmail.com>
2019-06-10 18:15:27 -0400

    Changed the import layout

--- unscramble.py
+++ unscramble.py
@@ -1,32 +1,5 @@
-try:
-    from subprocess import call
-    import itertools 
-    from nltk.corpus import wordnet
-except:
-    pythons = ['python -m ', 'py -m ', 'python3 -m ', 'py3 -m ']
-    pips = ['pip install ', 'pip3 install ']
-    #defined pip packages
-    pacakges = ['nltk']
-
-    for pip in pips:
-        for pacakge in pacakges:
-            for python in pythons:
-                try:
-                    call(python + pip + pacakge, shell = True)
-                except:
-                    print(python + pip + pacakge + ' is not a valid command')
-            
-            try:
-                call(pip + pacakge, shell = True)
-            except:
-                print(pip + pacakge + ' is not a valid command')
-    
-    from nltk.corpus import wordnet
-    import nltk
-    # nltk.download()
-    input('3rd party libries added please download the \'wordnet\' lexicon (press d)\n<Enter> to continue')
-                
-    
+import itertools 
+                    
 def load_dictonary():
     dictonary = []
     with open('english.txt', 'r') as file:
@@ -35,9 +8,6 @@ def load_dictonary():
             dictonary.append(line)
     
     return dictonary
-
-
-
 
 def main(word = 'ewlloh'):
     word = word.lower()
@@ -74,4 +44,4 @@ def main(word = 'ewlloh'):
 
 
 if __name__ == "__main__":
-    print(main(''))
+    print(main())
